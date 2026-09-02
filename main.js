80af1e804192a4b67d58c08fe6404cfbb6a13df8
Shivam <ahivampatel12@gmail.com>
2026-09-02 17:32:24 +0530

    Small Commit

--- main.js
+++ main.js
@@ -5,3 +5,13 @@ var swiper = new Swiper('.mySwiper', {
         prevEl: '#prev',
     },
 });
+
+
+const cartIcon= document.querySelector('.cart-icon');
+const cartTab= document.querySelector('.cart-tab');
+const closebtn= document.querySelector('.close-btn');
+
+
+cartIcon.addEventListener('click',()=> cartTab.classList.add('cart-tab-active'));
+closebtn.addEventListener('click',()=> cartTab.classList.remove('cart-tab-active'));
+
